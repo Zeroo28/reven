f1a7e93ac4a929a0aafdfe8bd75125ea940e42de
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2022-06-20 03:35:55 +0700

    Show dummy status item and made all widget building functions private

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:discord_rpc/utlis/constants.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,32 +14,53 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: buildTitle(theme),
+        title: _buildTitle(theme),
         leading: IconButton(
           hoverColor: Colors.transparent,
           splashColor: Colors.transparent,
-          splashRadius: 12,
+          splashRadius: null,
           onPressed: () {},
-          icon: const Icon(Icons.settings),
+          iconSize: 32,
+          icon: Icon(
+            Icons.settings,
+            color: (theme.buttonTheme.colorScheme?.background ??
+                    theme.textTheme.bodyText1?.color)
+                ?.withOpacity(.7),
+          ),
         ),
       ),
-      body: ListView(
-        children: const [],
-      ),
+      body: _buildBody(theme, context, size),
     );
   }
 
-  RichText buildTitle(ThemeData theme) {
+  Widget _buildBody(ThemeData theme, BuildContext context, Size size) {
+    return size.height > ProjectIntegers.minWindowHeight &&
+            size.width > ProjectIntegers.minWindowWidth
+        ? ListView(
+            children: [
+              _buildHeader(theme, context, size),
+            ],
+          )
+        : Center(
+            child: Text(
+              'Window too small!',
+              style: theme.textTheme.headline3,
+            ),
+          );
+  }
+
+  Widget _buildTitle(ThemeData theme) {
     return RichText(
       overflow: TextOverflow.fade,
       text: TextSpan(
         text: 'Discord',
         style: theme.appBarTheme.titleTextStyle?.copyWith(
           color: theme.colorScheme.onBackground,
-          fontWeight: FontWeight.bold,
+          fontWeight: FontWeight.w600,
         ),
         children: [
           TextSpan(
@@ -45,6 +69,40 @@ class _HomeState extends State<Home> {
               color: theme.colorScheme.onBackground,
               fontWeight: FontWeight.normal,
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(ThemeData theme, BuildContext ctx, Size size) {
+    final random = Random().nextBool();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 36),
+      child: Column(
+        children: [
+          Text(
+            'Status',
+            style: theme.textTheme.headline5?.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            width: size.width * .3,
+            child: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Text(
+                random ? 'Running' : 'Invisible',
+                style: theme.textTheme.headline1?.copyWith(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: random ? ProjectColors.success : theme.primaryColorDark,
+            height: 4,
+            width: size.width * .25,
           ),
         ],
       ),
