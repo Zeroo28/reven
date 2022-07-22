ecacb9d966a26a99fa39b557134e04880c1aa562
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2022-07-23 06:51:13 +0700

    fix: make logger class a singleton

--- lib/utils/helpers/logger_helper.dart
+++ lib/utils/helpers/logger_helper.dart
@@ -3,6 +3,13 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 
 class Logger {
+  static final Logger _instance = Logger._internal();
+  factory Logger() {
+    return _instance;
+  }
+
+  Logger._internal();
+
   void debug(dynamic message, {Object? error, StackTrace? stackTrace}) {
     if (kDebugMode) {
       log(message.toString(), error: error, stackTrace: stackTrace);
