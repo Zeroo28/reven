14e59028a21e768ac533024fe8104ea14745872d
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2022-07-22 22:39:50 +0700

    fix: make error and stacktrace params named params

--- lib/utils/helpers/logger_helper.dart
+++ lib/utils/helpers/logger_helper.dart
@@ -3,23 +3,23 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 
 class Logger {
-  void debug(dynamic message, [Object? error, StackTrace? stackTrace]) {
+  void debug(dynamic message, {Object? error, StackTrace? stackTrace}) {
     if (kDebugMode) {
       log(message.toString(), error: error, stackTrace: stackTrace);
     }
   }
 
-  void info(dynamic message, [Object? error, StackTrace? stackTrace]) {
+  void info(dynamic message, {Object? error, StackTrace? stackTrace}) {
     if (kDebugMode) {
       log(message.toString(), error: error, stackTrace: stackTrace);
     }
   }
 
-  void warn(dynamic message, [Object? error, StackTrace? stackTrace]) {
+  void warn(dynamic message, {Object? error, StackTrace? stackTrace}) {
     log(message.toString(), error: error, stackTrace: stackTrace);
   }
 
-  void error(dynamic message, [Object? error, StackTrace? stackTrace]) {
+  void error(dynamic message, {Object? error, StackTrace? stackTrace}) {
     log(message.toString(), error: error, stackTrace: stackTrace);
   }
 }
