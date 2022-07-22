78aaddc2650fc6847ada116609ecfb4bf77cf24a
Zeroo <77537889+Zeroo28@users.noreply.github.com>
2022-07-22 22:38:50 +0700

    docs: add comments to explain the code

--- lib/database/dao/application_dao.dart
+++ lib/database/dao/application_dao.dart
@@ -14,19 +14,45 @@ class ApplicationsDao extends DatabaseAccessor<AppDatabase>
 
   ApplicationsDao(AppDatabase db) : super(db);
 
+  /// Upsert an application in the database, i.e. insert item into the database
+  /// but update it on conflict.
+  ///
+  /// Params: required `Presence`.
+  ///
+  /// Returns: `Future<int>` index of updated column.
   Future saveApplication(Presence app) {
     final col = ApplicationCompanion(
       body: Value(app.toJsonString()),
       createdAt: Value(DateTime.now()),
     );
-    return into(application).insert(col);
+    return into(application).insertOnConflictUpdate(col);
   }
 
+  /// Query all applications from the database, no filtering. Use
+  /// `ApplicationsDao.getApplicationById` to get a single application with
+  /// matching id.
+  /// 
+  /// Returns `Future<List<Applications>>`, list will be empty if no applications
+  /// are found in the database.
   Future<List<Applications>> get getApplications => select(application).get();
 
-  Future<Applications> getApplicationsById(int id) => (select(application)
-        ..where(
-          (app) => app.id.equals(id),
-        ))
-      .getSingle();
+  /// Query single application from the database with matching ID.
+  /// 
+  /// Returns: `Future<Application>` or `null` if no applications are found.
+  Future<Applications>? getApplicationById(int id) {
+    try {
+      return (select(application)
+            ..where(
+              (app) => app.id.equals(id),
+            ))
+          .getSingle();
+    } catch (e, st) {
+      logger.error(
+        'Something went wrong in ApplicationsDao.getApplicationById.',
+        error: e,
+        stackTrace: st,
+      );
+      return null;
+    }
+  }
 }
